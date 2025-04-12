d0f908120beed0c09971805154c76a88f07023ae
Joice-Araujo <joice.aparecida21@hotmail.com>
2025-04-12 13:52:42 -0300

    :tada: feat: subindo projeto

---
+++ src/main/java/br/gov/sp/fatec/estudoSpringBoot2025/EstudoSpringBoot2025Application.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.estudoSpringBoot2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudoSpringBoot2025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudoSpringBoot2025Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/gov/sp/fatec/estudoSpringBoot2025/EstudoSpringBoot2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.estudoSpringBoot2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudoSpringBoot2025ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
